200b6ee9dbe339943d7eabdce3bc51c2963d7211
Kal-kali <92629719+Kal-kali@users.noreply.github.com>
2022-03-17 12:33:07 +0200

    Add files via upload

---
+++ oxy.py
@@ -0,0 +1,171 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pickle
+from time import sleep
+from pyrogram.errors import FloodWait
+import os
+from pyrogram import Client, filters, types, errors
+from pyrogram.types import ChatPermissions
+import time
+import random
+import asyncio
+from asyncio import sleep
+import re
+from io import BytesIO
+import requests
+import base64
+from time import perf_counter
+import subprocess
+import wikipedia
+from gtts import gTTS
+from covid import Covid
+from pyrogram.raw import functions
+from datetime import datetime
+import webbrowser 
+import json
+
+
+
+app = Client('cmd', api_id=15897262, api_hash='90476d9c65a86b03837e1e249314cd75')
+
+
+
+
+app.start()
+
+app.stop()
+if os.sys.platform == "win32":
+    os.system("cls")
+else:
+    os.system("clear")
+print('''   v0.8
+     
+   ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+   ┃▒▄█▀▀█▒█▀█▀█─░▄█▀▄─▒▐█▀▀▄▒▐█▀▀▀█▌░▐█▀▀░▐█▀█▄┃
+   ┃▒▀▀█▄▄░░▒█░░░▐█▄▄▐█▒▐█▒▐█░▒▄▄█▀▀░░▐█▀▀░▐█▌▐█┃
+   ┃▒█▄▄█▀░▒▄█▄░░▐█─░▐█▒▐█▀▄▄▒▐█▄▄▄█▌░▐█▄▄░▐█▄█▀┃
+   ┃---------Telegram: @starzedscripts----------┃
+   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+''')
+
+print("После ввода задержки напишите в любой телеграм чат команду /help для просмотра команд!")
+print("\n Just Relax уебан!")
+print()
+
+cool = int(input("Введите завис.число - от него будет зависеть скорость (Норма 8):  "))
+
+global number
+number = 0
+
+while cool == 0:
+    print("Слишком быстро!")
+    cool = int(input("Введите завис.число - от него будет зависеть скорость (Норма 8):  "))
+
+while cool == 1:
+    print("Слишком быстро!")
+    cool = int(input("Введите завис.число - от него будет зависеть скорость (Норма 8):  "))
+
+while cool == 2:
+    print("Слишком быстро!")
+    cool = int(input("Введите завис.число - от него будет зависеть скорость (Норма 8):  "))
+
+while cool > 10:
+    print("Слишком медленно!")
+    cool = int(input("Введите завис.число - от него будет зависеть скорость (Норма 8):  "))
+
+while cool < 0:
+    print("ОЧЕНЬ БЫСТРО........")
+    cool = int(input("Введите завис.число - от него будет зависеть скорость (Норма 8):  "))
+
+  
+
+  
+@app.on_message(filters.command("oxy", prefixes=".") & filters.me)
+def hent(Client,message):
+    app.send_message(message.chat.id, f'''<b>ГОВНО</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b> ЗАЛУПА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ПЕНИС</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ХЕР</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ДАВАЛКА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ХУЙ</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>БЛЯДИНА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ГОЛОВКА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ШЛЮХА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ЖОПА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ЧЛЕН</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ЕБЛАН</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ПЕТУХ</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>МУДИЛА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>РУКОБЛУД</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ССАНИНА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ОЧКО</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>БЛЯДУН</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ВАГИНА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>СУКА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ЕБЛАНИЩЕ</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ВЛАГАЛИЩЕ</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ПЕРДУН</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ДРОЧИЛА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ПИДОР</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ПИЗДА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ТУЗ</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>МАЛАФЬЯ</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ГОМИК</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>МУДИЛА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ПИЛОТКА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>МАНДА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>АНУС</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ВАГИНА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ПУТАНА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ПЕДРИЛА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ШАЛАВА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ХУИЛА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>МОШОНКА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>ЕЛДА</b>''')
+    sleep(1.0)
+    app.send_message(message.chat.id,f'''<b>РАУНД!!</b>''')
+    sleep(1.0)
+
+app.run()
+
